ecde6dc09af5da5252f564061836e6894e3cf60a
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2023-03-15 08:54:27 +0100

    added 2 quick tests

---
+++ Practice_0/Python_Basics/index_of.py
@@ -0,0 +1,4 @@
+
+randomlist = ["first", "second", "third"]
+a = randomlist.index("second")
+print(a) # if not not in list, value error will be thrown
---
+++ Practice_0/Python_Basics/swap_values.py
@@ -0,0 +1,9 @@
+
+a = 20
+b = 10
+print(a)
+print(b)
+
+a, b = b, a
+print(a)
+print(b)
